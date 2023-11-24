c4fde9f6fe7148c441cbaba2d95cfe90218b278c
mingodad <mingodad@gmail.com>
2023-11-24 11:58:33 +0100

    Added MetaDSL grammar

--- playground/index.js
+++ playground/index.js
@@ -146,6 +146,7 @@ const sampleList = [
 	["Menhir stage1 parser", "menhir-stage1-parser.g", "menhir-stage1-parser.mly", "ace/mode/yaml"],
 	["Menhir fancy parser", "menhir-fancy-parser.g", "menhir-fancy-parser.mly", "ace/mode/yaml"],
 	["Menhir fancy parser left recursion", "menhir-fancy-parser-left-rec.g", "menhir-fancy-parser.mly", "ace/mode/yaml"],
+	["MetaDSL parser", "MetaDSL.g", "test.MetaDSL", "ace/mode/txt"],
 	["Minic parser", "minic.g", "test.minic", "ace/mode/c_cpp"],
 	["Minizinc parser (not working)", "minizinc.g", "test.mzn", "ace/mode/text"],
 	["MSTA parser", "msta.g", "test.msta", "ace/mode/yaml"],
