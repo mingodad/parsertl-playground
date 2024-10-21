7d5132a40501f38a9d167b662e1d5ac381871d5a
mingodad <mingodad@gmail.com>
2024-10-21 13:02:06 +0200

    Added Quakeforge grammar

--- playground/index.js
+++ playground/index.js
@@ -261,6 +261,7 @@ const sampleList = [
 	["Protobuf3 parser", "pb_parsing_parser.g", "test.proto3", "ace/mode/c_cpp"],
 	["Protocompile parser", "protocompile.g", "test.protocompile", "ace/mode/c_cpp"],
 	["Pyethon parser", "pyethon.g", "test.pyethon", "ace/mode/python"],
+	["Quakeforge QC parser (partially working)", "quakeforge-qc-parse.g", "test.quakeforge-qc-parse", "ace/mode/c_cpp"],
 	["ReasonML parser (partially working)", "reason_parser.g", "test.reason", "ace/mode/javascript"],
 	["R parser (partially working)", "r-parser.g", "test.r-parser", "ace/mode/r"],
 	["Re2c parser (partially working)", "re2cr.g", "test.re2c", "ace/mode/text"],
--- playground/test-all.sh
+++ playground/test-all.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 for fn in *.g
 do
-	echo "== Now testing $fn"
+	#echo "== Now testing $fn"
+	echo "$fn:1:1:"
 	/usr/bin/time ./parsertl-playground $fn test.empty
 done
